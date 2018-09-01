61680ad18a0a142a0846b9b6a0a2f05e8c79fd27
BalajiMariappan <mbala38@hotmail.com>
2018-09-01 19:03:04 +1000

    Program.cs code added to run and see the results

--- dotnet-code-challenge/Program.cs
+++ dotnet-code-challenge/Program.cs
@@ -6,9 +6,21 @@ namespace dotnet_code_challenge
     {
         private static void Main(string[] args)
         {
-            
+            try
+            {
+                string wolfpath = ".\\FeedData\\Wolferhampton_Race1.json";
+                string Caulfiepath = @".\\FeedData\\Caulfield_Race1.xml";
+                WolferHamptoncs.GetWolferHamptonFData(wolfpath);
+                CaulfieldRace.GetCaulfieldRace(Caulfiepath);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
 
-           
+            Console.ReadLine();
+
+
         }
     }
 }
